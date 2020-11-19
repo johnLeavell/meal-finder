502ea7d99f8ca8e2948594fc145931d699f9ee7b
John Leavell <johnleavell@gmail.com>
2020-11-18 20:48:41 -0600

    finished search function

--- script.js
+++ script.js
@@ -27,7 +27,24 @@ function searchMeal(e){
         .then(res => res.json())
         .then(data => {
             console.log(data);
+            resultHeading.innerHTML = `<h2>Search Results for '${term}':</h2>`
+
+            if(data.meals == null) {
+                resultHeading.innerHTML = `<p>There are no search results. Try again!</p>`;
+            } else {
+                mealsEl.innerHTML = data.meals.map(meal => `
+                <div class="meal">
+                    <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+                    <div class="meal-info" data-mealId="${meal.idMeal}">
+                        <h3>${meal.strMeal}</h3>
+                    </div>
+                </div>`
+                )
+                .join('');
+            }
         })
+        // clear search text
+        search.value = '';
     } else {
         alert('Please enter a search value and term')
     }
