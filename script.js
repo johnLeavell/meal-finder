0d10adc14e2c36e310b4a97b22c20a721559e1ec
John Leavell <johnleavell@gmail.com>
2020-11-19 10:25:26 -0600

    added show single meal page feature

--- script.js
+++ script.js
@@ -50,8 +50,64 @@ function searchMeal(e){
     }
 }
 
+//fetch meal by ID
+function getMealById(mealID) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then(res => res.json())
+    .then(data => {
+        // console.log(data)
+        const meal = data.meals[0]
 
+        addMealToDOM(meal);
+    })
+}
 
+// add meal to dom
+function addMealToDOM(meal) {
+    const ingredients = [];
+     for(let i = 1; i <= 20; i++) {
+        if(meal[`strIngredient${i}`]) {
+            ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`)
+        } else{
+            break;
+        }
+    }
+
+    single_mealEl.innerHTML = `
+        <div class="single-meal">
+            <h1>${meal.strMeal}</h1>
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+            <div class="single-meal-info">
+                ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+                ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+            </div>
+            <div class="main">
+                <p>${meal.strInstructions}</p>
+                <h2>Ingredients</h2>
+                <ul>
+                    ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+                </ul>
+            </div>
+        </div>
+    `
+}
 
 // event listeners
  submit.addEventListener('submit', searchMeal)
+
+ mealsEl.addEventListener('click', e => {
+     const mealInfo = e.path.find(item =>{
+        //  console.log(item);
+        if(item.classList) {
+            return item.classList.contains('meal-info');
+        } else{
+            return false;
+        }
+     })
+    //  console.log(mealInfo);
+    if(mealInfo) {
+        const mealID = mealInfo.getAttribute('data-mealid');
+        // console.log(mealID);
+        getMealById(mealID)
+    }
+ })
